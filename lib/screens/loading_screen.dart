2400b6b5b277494fde0ef753ecddba31283ac402
sk-weepet <67887045+sk-weepet@users.noreply.github.com>
2020-07-13 21:58:57 +0900

    162. Refactoring Location Methods

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 // import 'package:geolocator/geolocator.dart';
-import 'package:clima/services/location.dart';
-import 'package:clima/services/networking.dart';
+
 import 'location_screen.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = '231bc96334d3e4fd2756459ecb38dccd';
+import 'package:clima/services/weather.dart';
 
 class LoadingScreen extends StatefulWidget {
   // Loading Screen ???????
@@ -21,12 +19,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper('https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(
