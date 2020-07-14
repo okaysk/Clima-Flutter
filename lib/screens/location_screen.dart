c711f883308a0010ec2fd674c8fd59b3424df9d0
sk-weepet <67887045+sk-weepet@users.noreply.github.com>
2020-07-14 14:21:53 +0900

    Final

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -82,10 +82,16 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    onPressed: () async {
+                      var typedName = await Navigator.push(context, MaterialPageRoute(builder: (context) {
                         return CityScreen();
                       }));
+                      if (typedName != null) {
+                        // very important to check!! 사용자가 아무것도 입력 안할 수도 있으니께
+                        var weatherData = await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
+                      print(typedName);
                     },
                     child: Icon(
                       Icons.location_city,
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,13 @@ const apiKey = '231bc96334d3e4fd2756459ecb38dccd';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData(); // await!!
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
